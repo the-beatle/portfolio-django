fc6a4a82c56ae9915089b888ba8245a2133a9abe
Mario <marcae7@gmail.com>
2022-01-20 22:33:41 -0500

    Revert "nested serializer for MPTT"
    This reverts commit 6b37de0730611afdaa130cbd9639b045d2a2705c.

--- category/serializers.py
+++ category/serializers.py
@@ -4,12 +4,6 @@ from category.models import Category
 
 
 class CategorySerializer(serializers.ModelSerializer):
-    children = serializers.SerializerMethodField()
-
     class Meta:
-        depth = 1
         model = Category
-        fields = ("id","name","children")
-
-    def get_children(self, obj):
-        return CategorySerializer(obj.get_children(), many=True).data
+        fields = "__all__"
